bda6aec99fac34a576eeb683870033b12d6ad373
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-23 09:09:09 -0600

    Initial Project Creation / Cleanup

---
+++ MovieSearchC/MovieSearchC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieSearchC
+//
+//  Created by Bradley GIlmore on 6/23/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
